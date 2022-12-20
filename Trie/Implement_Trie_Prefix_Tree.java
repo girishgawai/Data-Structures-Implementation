58357dae01fe944c3a1ed291579a0ec5afff0cc4
Girish Vishnu Gawai <111906891+girishgawai@users.noreply.github.com>
2022-12-20 18:33:15 +0530

    Add files via upload

---
+++ Trie/Implement_Trie_Prefix_Tree.java
@@ -0,0 +1,80 @@
+package trie.qps;
+
+import java.util.HashMap;
+
+public class Implement_Trie_Prefix_Tree {
+	
+	private class Node {
+		int data;
+		HashMap<Character, Node> children = new HashMap<>();
+		boolean isTerminal = false;
+	}
+	
+	private Node root;
+	
+	public Implement_Trie_Prefix_Tree()
+	{
+		Node nn = new Node();
+		nn.data = '*';
+		this.root = nn;
+	}
+	
+	public void insert(String word)
+	{
+		Node node = root;
+		for (int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			
+			if(node.children.containsKey(ch))
+			{
+				node = node.children.get(ch);
+			}
+			else
+			{
+				Node nn = new Node();
+				nn.data = ch;
+				node.children.put(ch, nn);
+				node = nn;
+			}
+		}
+		
+		node.isTerminal = true;
+	}
+	
+	public boolean search(String word)
+	{
+		Node node = root;
+		for (int i = 0; i < word.length(); i++) {
+			char ch = word.charAt(i);
+			if(node.children.containsKey(ch))
+			{
+				node = node.children.get(ch);
+			}
+			else
+			{
+				return false;
+			}
+		}
+		
+		return node.isTerminal;
+	}
+	
+	public boolean startsWith(String prefix)
+	{
+		Node node = root;
+		for (int i = 0; i < prefix.length(); i++) {
+			char ch = prefix.charAt(i);
+			if(node.children.containsKey(ch))
+			{
+				node = node.children.get(ch);
+			}
+			else
+			{
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+}
